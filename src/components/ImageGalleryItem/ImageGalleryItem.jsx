e770f37669eff3a78945bf3dc11f9e5ab322c3a6
Vlad Pazyna <vladpazyna@gmail.com>
2023-01-29 15:21:20 +0200

    in progress

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,8 +1,8 @@
 import css from './image-gallery-item.module.scss';
 import PropTypes from 'prop-types';
 
-const ImageGalleryItem = () => {
-  return (
+const ImageGalleryItem = ({ items }) => {
+  const elements = (
     <li className={css.gallery_item}>
       <img src="" alt="" />
     </li>
